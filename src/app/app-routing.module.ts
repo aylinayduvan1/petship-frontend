3ad1247f37080127a4a8639494178ee1af5dfbc0
thana <caglaozyy@gmail.com>
2023-06-19 23:12:54 +0300

    Profile-component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LogincComponent } from './loginc/loginc.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: LogincComponent },
   { path: 'homepage', component: HomepageComponent },
   { path: 'hakkimizda', component: AboutComponent, pathMatch: 'full' },
+  { path: 'profile' , component: ProfileComponent, }
   ];
 
 @NgModule({
