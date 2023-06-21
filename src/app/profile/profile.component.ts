1e3f58cf03a4251121b57f902a64f5f002bd0878
aylinayduvan <aylinnayduvann@gmail.com>
2023-06-21 10:43:38 +0300

    profile fix -scroll and punto

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, Input, ElementRef, OnInit, ViewChild, AfterViewInit , HostListener, EventEmitter, Output} from '@angular/core';
+import { AnimationItem } from 'lottie-web'; 
+declare let lottie: any;
+
 
 @Component({
   selector: 'app-profile',
@@ -30,4 +33,39 @@ export class ProfileComponent implements OnInit {
   } 
 
  
+  //up button
+@ViewChild('animationContainer', { static: true }) private animationContainer!: ElementRef;
+private animationInstance!: AnimationItem;
+isScrolledToTop: boolean = true;
+isScrollingDown: boolean = false;
+
+ngAfterViewInit(): void {
+  const animationOptions: any = {
+    container: this.animationContainer.nativeElement,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'assets/upbutton.json' // Lottie animasyonunun yolu
+  };
+
+  this.animationInstance = lottie.loadAnimation(animationOptions);
+}
+
+@HostListener('window:scroll')
+onWindowScroll(): void {
+  const scrollOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+  this.isScrollingDown = scrollOffset > 0;
+  this.isScrolledToTop = scrollOffset === 0;
+}
+
+scrollToTop(): void {
+  if (this.isScrolledToTop) {
+    return;
+  }
+
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
 }
