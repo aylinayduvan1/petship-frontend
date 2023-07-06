da390844ddbe889eda4b90c15e0f82c2bb75d734
caglaozy <caglaozyy@gmail.com>
2023-07-06 11:21:19 +0300

    advert-html edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,6 +38,19 @@ import { MultiSelectModule } from 'primeng/multiselect';
 import { CustomerService } from './components/pagess/profile/profile-panel/categories-listes/type-service/customer_service';
 import { DropdownModule } from 'primeng/dropdown';
 import { ConfirmationService} from 'primeng/api';
+import { ProductService } from './components/pagess/profile/profile-panel/advert-listes/service/productservice';
+import { Toolbar, ToolbarModule } from 'primeng/toolbar';
+
+
+
+
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { FileUploadModule } from 'primeng/fileupload';
+import { RatingModule } from 'primeng/rating';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 
 @NgModule({
@@ -85,12 +98,23 @@ import { ConfirmationService} from 'primeng/api';
      DropdownModule,
      AccordionModule,
     ButtonModule,
-     ToastModule
+     ToastModule,
+     ToolbarModule,
+     ConfirmDialogModule,
+     FileUploadModule,
+    RatingModule,
+    TagModule,
+    InputTextModule,
+    InputTextareaModule,
+    DropdownModule,
+    RadioButtonModule,
+    InputNumberModule,
   ],
   providers: [AuthService,
     CustomerServiceUserList,
     MessageService,
     CustomerService,
+    ProductService,
     AuthModule],
   bootstrap: [AppComponent]
 
--- src/app/components/pagess/profile/profile-panel/advert-listes/domain/product.ts
+++ src/app/components/pagess/profile/profile-panel/advert-listes/domain/product.ts
@@ -4,9 +4,6 @@ export interface Product {
   code?: string;
   name?: string;
   description?: string;
-  price?: number;
-  quantity?: number;
-  inventoryStatus?: string;
   category?: string;
   image?: string;
   rating?: number;
