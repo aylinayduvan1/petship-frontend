b593bf165b7ccf81b3fc28fda0a5168fb86d50ad
muhammedsafatur <msafatur@outlook.com>
2023-07-14 10:16:09 +0300

    user-info-listes component updated

--- src/app/components/pagess/profile/profile-panel/users-info-listes/users-info-listes.component.ts
+++ src/app/components/pagess/profile/profile-panel/users-info-listes/users-info-listes.component.ts
@@ -1,11 +1,13 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { UserService } from './service/user.service';
+import { User } from './../../../../../models/user.model';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api/api.service';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 @Component({
   selector: 'app-users-info-listes',
   templateUrl: './users-info-listes.component.html',
   styleUrls: ['./users-info-listes.component.css']
 })
-export class UsersInfoListesComponent {
+export class UsersInfoListesComponent implements OnInit {
   @Output() contentChange: EventEmitter<string> = new EventEmitter<string>();
   currentContent: string = 'home';
 
@@ -16,17 +18,20 @@ export class UsersInfoListesComponent {
   selectedOption1: string = '';
   selectedOption2: string = '';
   selectedGender: string = '';
-  ad: string = '';
-  soyad: string = '';
-  email: string = '';
-  bdate: string = '';
-  gsm: string = '';
-  gender: string = '';
+  constructor(private readonly apiService: ApiService, private router: Router) { }
 
-  constructor() { }
+  users: User[] = [];
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.refresh();
 
+  }
+
+  refresh() {
+    this.apiService.getAllEntities(User).subscribe((response) => {
+      this.users = response.data;
+      console.log(this.users)
+    });
   }
 
   scrollToSection(section: string) {
@@ -46,7 +51,6 @@ export class UsersInfoListesComponent {
 
   update() {
     // Güncelleme işlemleri
-
     console.log('Seçili cinsiyet:', this.selectedGender);
   }
   animalHistoryOptions = [
@@ -60,6 +64,3 @@ export class UsersInfoListesComponent {
   ];
 
 }
-
-
-
